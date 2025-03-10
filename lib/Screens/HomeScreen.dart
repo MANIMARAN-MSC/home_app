5b1376fe2f0596fe6157c2ce27df5444bc39a41a
MANIMARAN-MSC <kirubamani2002@gmail.com>
2025-03-10 15:15:32 +0530

    home Screen added
    home Screen file

---
+++ lib/Screens/HomeScreen.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'CustomDrawer.dart';
+import 'custom_drawer.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      drawer: CustomDrawer(),
+      body: Center(
+        child: Text(
+          "Welcome to the Home Screen!",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
